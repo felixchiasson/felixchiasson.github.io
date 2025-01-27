5c0874bde7aa1bf467cd05a3d8f0ff704cadba54
Félix Chiasson <github@felixchiasson.ca>
2025-01-26 21:08:53 -0500

    I don't want any of this anymore

--- _includes/javascript/rand_bg.js
+++
@@ -1,2 +0,0 @@
-var images = ['http://i.imgur.com/1BB0H0f.jpg', 'http://i.imgur.com/HoGO3uN.jpg'];
-document.getElementById("bg-text").style.background-image = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
